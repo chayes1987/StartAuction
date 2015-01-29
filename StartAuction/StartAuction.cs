eca1880f461cf9dc35d456e8487dcb1939670018
Conor Hayes <ch1987@live.ie>
2015-01-29 20:24:53 +0000

    Added library for Redis using NuGet, made a test connection to the database

--- StartAuction/StartAuction.cs
+++ StartAuction/StartAuction.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StackExchange.Redis;
 
 namespace StartAuction
 {
     class StartAuction
     {
+        private const string SERVER_NAME = "localhost";
+        private const int NAMESPACE = 0;
+
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(SERVER_NAME);
+            IDatabase database = connection.GetDatabase(NAMESPACE);
+            Console.WriteLine(database.Database);
+            Console.ReadLine();
+        }
     }
 }
