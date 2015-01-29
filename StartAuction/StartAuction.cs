ffb093152146356e1cc86a06756f52bd617b6b9b
Conor Hayes <ch1987@live.ie>
2015-01-29 20:38:22 +0000

    Added library for 0mq using NuGet, connected to 0mq and subscribed to StartAuction command, received the command successfully

--- StartAuction/StartAuction.cs
+++ StartAuction/StartAuction.cs
@@ -4,20 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using StackExchange.Redis;
+using NetMQ;
 
 namespace StartAuction
 {
     class StartAuction
     {
-        private const string SERVER_NAME = "localhost";
+        private const string SUBSCRIBER_ADDRESS = "tcp://127.0.0.1:1000", TOPIC = "StartAuction", SERVER_NAME = "localhost";
         private const int NAMESPACE = 0;
+        private NetMQContext context = NetMQContext.Create();
 
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(SERVER_NAME);
             IDatabase database = connection.GetDatabase(NAMESPACE);
             Console.WriteLine(database.Database);
-            Console.ReadLine();
+
+            new StartAuction().subscribe();
+        }
+
+        private void subscribe()
+        {
+            var subscriber = context.CreateSubscriberSocket();
+            subscriber.Connect(SUBSCRIBER_ADDRESS);
+            subscriber.Subscribe(TOPIC);
+            Console.WriteLine("Subscribed to " + TOPIC + " command...");
+
+            while (true)
+            {
+                string command = subscriber.ReceiveString();
+                Console.WriteLine("Received command: " + command);
+            }
         }
     }
 }
