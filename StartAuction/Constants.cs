5a34b5baeec875e10b7e3da25c7b00ff0bed58f8
Conor Hayes <ch1987@live.ie>
2015-02-19 23:37:56 +0000

    Bugfix, acknowlegdement topic naming

--- StartAuction/Constants.cs
+++ StartAuction/Constants.cs
@@ -11,7 +11,7 @@ namespace StartAuction
         public const int REDIS_NAMESPACE = 0;
         public const string START_AUCTION_TOPIC = "StartAuction";
         public const string AUCTION_STARTED_ACK_TOPIC = "ACK: AuctionStarted";
-        public const string NOTIFY_BIDDERS_ACK_TOPIC = "ACK: NotifyBidders";
+        public const string NOTIFY_BIDDERS_ACK_TOPIC = "ACK: NotifyBidder";
         public const string START_AUCTION_ADR = "tcp://172.31.32.20:1001";
         public const string NOTIFY_BIDDERS_ACK_ADR = "tcp://172.31.32.22:1010";
         public const string PUB_ADR = "tcp://*:1011";
