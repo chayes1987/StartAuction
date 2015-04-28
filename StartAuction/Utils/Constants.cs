efe5dda67349d5e69f7a27b423a3334e5b095419
Conor Hayes <ch1987@live.ie>
2015-04-28 02:37:06 +0100

    Tidy up

--- StartAuction/Utils/Constants.cs
+++ StartAuction/Utils/Constants.cs
@@ -5,9 +5,19 @@ using Auction.Database;
 
 namespace Auction.Utils
 {
+    /// <summary>
+    /// Constants
+    /// </summary>
     public class Constants
     {
+        /// <summary>
+        /// Broker
+        /// </summary>
         public static MessageBroker BROKER = MessageBroker.ZeroMq;
+        
+        /// <summary>
+        /// Database
+        /// </summary>
         public static DatabaseType DATABASE = DatabaseType.Redis;
     }
 }
